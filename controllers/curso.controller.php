f48056df6fa3e6a1f0fc5bc79bd24f90f7302fd8
NestorPQ <1208003@senati.pe>
2023-04-04 12:02:28 -0500

    Listar MVC terminado

--- controllers/curso.controller.php
+++ controllers/curso.controller.php
@@ -6,7 +6,37 @@ if (isset($_POST['operacion'])){
   $curso = new Curso();
 
   if ($_POST['operacion'] == 'listar'){
-    $datosObtenidos = $curso->listarCursos();
-    echo json_encode($datosObtenidos);
+    $datosObtenidos = $curso ->listarCursos();
+    //  En esta ocasión NO enviaremos un objero JSON, en su lugar
+    //  el controlador renderizará las filas que necesita <tbody></tbodt>
+    //  echo json_encode($datosObtenidos);
+
+
+    //PASO 1: Verificar que el objeto contenga datos
+    if ($datosObtenidos) {
+      $numeroFila = 1;
+      //  PASO 2: Recorrer todo el objeto
+      foreach($datosObtenidos as $curso){
+        //  PASO3 3: Ahora construimos las filas
+        echo "
+        <tr>
+          <td>{$numeroFila}</td>
+          <td>{$curso['nombrecurso']}</td>
+          <td>{$curso['especialidad']}</td>
+          <td>{$curso['complejidad']}</td>
+          <td>{$curso['fechainicio']}</td>
+          <td>{$curso['precio']}</td>
+          <td>
+            <a href='#' class='btn btn-danger btn-sm'><i class='bi bi-trash'></i></a>
+
+            <a href='#' class='btn btn-success btn-sm'><i class='bi bi-pencil'></i></a>
+          </td>
+
+          
+          </tr>
+        ";
+        $numeroFila++;
+      }
+    }
   }
 }
